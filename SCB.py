17699b30fe16bc7e1e721ed6ae8aaef2d879652a
alvsgabriellk <cauagabriell81@gmail.com>
2025-05-31 10:48:21 -0300

    Correções

--- SCB.py
+++ SCB.py
@@ -1,6 +1,6 @@
 import random
 
-print('-=-=-=-=-= BANCO -=-=-=-=-=')
+print('-=-=-=-=-=-=-=- BANCO -=-=-=-=-=-=-=-')
 print('''[ 1 ] | CRIAR CONTA
 [ 2 ] | DEPOSITAR
 [ 3 ] | SACAR
@@ -8,28 +8,40 @@ print('''[ 1 ] | CRIAR CONTA
 [ 5 ] | CONSULTAR SALDO
 [ 6 ] | LISTAR TODAS AS CONTAS
 [ 7 ] | ENCERRAR CONTA''')
-print('-=-=-=-=-= BANCO -=-=-=-=-=')
-opção = int(input('Qual a sua opção?'))
+print('-=-=-=-=-=-=-=- BANCO -=-=-=-=-=-=-=-')
+opção = int(input('-----> Qual a sua opção? '))
 while opção != 7:
     if opção == 1:
-        nome = str(input('Digite aqui seu nome completo:')).strip()
+        print('-'*50)
+        nome = str(input('Digite aqui seu nome completo: ')).strip()
         def pegar_cpf():
             while True:
-                cpf = str(input('Digite o seu CPF:'))
+                cpf = input('Digite o seu CPF: ').strip()
                 if len(cpf) == 11 and cpf.isdigit():
-                    print('CPF Aceito!')
+                    print('--> CPF Aceito! ')
                     return cpf
                 else:
-                    print('CPF inválido!, tem que ter exatamente 11 digitos.')
-            cpf_usuario = cpf
-        pegar_cpf()
-        def formatar_cpf(cpf_usuario = pegar_cpf()):
+                    print('--> CPF inválido!')
+        def formatar_cpf(cpf_usuario):
             return f'{cpf_usuario[0:3]}.{cpf_usuario[3:6]}.{cpf_usuario[6:9]}-{cpf_usuario[9:11]}'
-        cpf_formatado = cpf_formatado(cpf_usuario)
-        formatar_cpf()
-        gerador_numero_conta = random.randint(1000, 7000)
-
-y
+        cpf_usuario = pegar_cpf()
+        cpf_formatado = formatar_cpf(cpf_usuario)
+        gerador_numero_conta = random.randint(1000, 9999)
+        print('='*45)
+        print('Nome do usuário: {}'.format(nome))
+        print('CPF: {}'.format(cpf_formatado))
+        print('Código da conta: {}'.format(gerador_numero_conta))
+        print('-'*50)
+    print('-=-=-=-=-=-=-=- BANCO -=-=-=-=-=-=-=-')
+    print('''    [ 1 ] | CRIAR CONTA
+    [ 2 ] | DEPOSITAR
+    [ 3 ] | SACAR
+    [ 4 ] | TRANSFERIR
+    [ 5 ] | CONSULTAR SALDO
+    [ 6 ] | LISTAR TODAS AS CONTAS
+    [ 7 ] | ENCERRAR CONTA''')
+    print('-=-=-=-=-=-=-=- BANCO -=-=-=-=-=-=-=-')
+    opção = int(input('-----> Qual a sua opção? '))
 
 
 
